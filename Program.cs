752e050d10b56f7d6f6cdb2610488bae5ea62a9b
sagar <skatahre@gmail.com>
2022-10-28 01:21:09 +0530

    welcome to snake and ladder

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Snake
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Problem");
+        }
+    }
+}
