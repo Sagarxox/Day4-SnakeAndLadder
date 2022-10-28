97f3b586b300d479b1f6d96f129796a72ce2b8b3
sagar <skatahre@gmail.com>
2022-10-28 13:22:20 +0530

    winning position 100.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@ namespace Snake
     public class Program
     {
         static void Main(string[] args)
-        { 
+        {
             
             Snake snake = new Snake();
+           
+
+               snake.Start();
             
-            snake.CheckforOption();
         }
+
 
     }
 }
