5d13a3d4ca88a14090d326a195413d4a929e1c40
sagar <skatahre@gmail.com>
2022-10-28 12:17:54 +0530

    dice roll

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ namespace Snake
 {
     public class Program
     {
-        
-            static void Main(string[] args)
+        static void Main(string[] args)
+        { 
+            
+            Snake snake = new Snake();
+            
             {
-                Snake snake = new Snake();
-                Console.WriteLine("Initial Position is {0}", snake.initialPosition);
+                Console.WriteLine("get number After Dice Roll is {0}", snake.DiceRoll());
             }
-        
+        }
+
     }
 }
